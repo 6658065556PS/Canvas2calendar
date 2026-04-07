66f8018a5a82b9004096417452797a91dcb241b7
Yvonne <yvonne@Yvonnes-MacBook-Pro-4.local>
2026-04-07 11:47:03 -0700

    Use desktop logo image in shared Logo component

--- src/app/components/Logo.tsx
+++ src/app/components/Logo.tsx
@@ -1,26 +1,19 @@
 import type { ComponentPropsWithoutRef } from "react";
+import logoImg from "../assets/logo.png";
 
-interface LogoProps extends ComponentPropsWithoutRef<"svg"> {
+interface LogoProps extends ComponentPropsWithoutRef<"img"> {
   size?: number;
 }
 
 export function Logo({ size = 32, className, ...props }: LogoProps) {
   return (
-    <svg
-      viewBox="0 0 72 72"
+    <img
+      src={logoImg}
+      alt="CalDaily logo"
       width={size}
       height={size}
       className={className}
-      aria-hidden="true"
       {...props}
-    >
-      <path d="M8 30h56v18l-8 10H16l-8-10V30Z" fill="#FDB515" />
-      <path d="M8 30h56v-18a4 4 0 0 0-4-4H14a4 4 0 0 0-4 4v18Z" fill="#003262" />
-      <rect x="10" y="6" width="12" height="12" rx="5" fill="#003262" />
-      <rect x="30" y="6" width="12" height="12" rx="5" fill="#003262" />
-      <rect x="50" y="6" width="12" height="12" rx="5" fill="#003262" />
-      <path d="M36 36c-4.4 0-8 3.6-8 8v6h16v-6c0-4.4-3.6-8-8-8Z" fill="#003262" />
-      <path d="M36 40a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-1 6v4h2v-4h-2Z" fill="#FDB515" />
-    </svg>
+    />
   );
 }
